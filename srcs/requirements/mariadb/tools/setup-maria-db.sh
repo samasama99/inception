de401c6fd72e6f18144ce47742a25910101bfc0b
sama <unlimit8.com@gmail.com>
2023-02-04 19:13:38 +0100

    some mariadb bs

--- srcs/requirements/mariadb/tools/setup-maria-db.sh
+++ srcs/requirements/mariadb/tools/setup-maria-db.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-#VARIABLES::
+#
+#VARIABLES:
+# PASSWORD="azerty1234"
+#CMDS:
+# sed -i '19s/./port         = 3306/' /etc/mysql/mariadb.conf.d/50-server.cnf
+# sed -i '28s/./bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed 's/#port                   = 3306/port                   = 3306/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed 's/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-PASSWORD="azerty1234"
 
-#CMDS::
-"\nny" | sudo mysql_secure_installation
+service  mysql start 
 
+mariadb -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
+mariadb -e "FLUSH PRIVILEGES"
+
+echo "FINISHED!"
