8de9a9bf2e040082fa33990154204e64349b0706
sama <unlimit8.com@gmail.com>
2023-02-06 18:06:21 +0100

    the first step of nginx and wordpress

--- srcs/requirements/mariadb/tools/setup-maria-db.sh
+++ srcs/requirements/mariadb/tools/setup-maria-db.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-#
-#VARIABLES:
-# PASSWORD="azerty1234"
-#CMDS:
-# sed -i '19s/./port         = 3306/' /etc/mysql/mariadb.conf.d/50-server.cnf
-# sed -i '28s/./bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-sed 's/#port                   = 3306/port                   = 3306/' /etc/mysql/mariadb.conf.d/50-server.cnf
-sed 's/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+sed -i 's/#port                   = 3306/port                   = 3306/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 
 service  mysql start 
 
-mariadb -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
+mariadb -e "UPDATE mysql.user SET Password = PASSWORD('azerty999') WHERE User = 'root'"
 mariadb -e "FLUSH PRIVILEGES"
 
-echo "FINISHED!"
+exec mysqld_safe
