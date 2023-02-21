5f7c83fea91f99ba5483b0dad6d4ba9594b60976
sama <unlimit8.com@gmail.com>
2023-02-21 18:21:12 +0100

    mariadb and adminer working

--- srcs/requirements/mariadb/tools/setup-maria-db.sh
+++ srcs/requirements/mariadb/tools/setup-maria-db.sh
@@ -4,9 +4,34 @@ sed -i 's/#port                   = 3306/port                   = 3306/' /etc/my
 sed -i 's/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 
-service  mysql start 
 
-mariadb -e "UPDATE mysql.user SET Password = PASSWORD('azerty999') WHERE User = 'root'"
-mariadb -e "FLUSH PRIVILEGES"
+if mariadb -e "USE wordpress" ; then
+    echo "Database 'wordpress' already exists."
+else
+    service mysql start
+    echo "CREATE DATABASE wordpress;" | mysql
+    echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY 'pass' WITH GRANT OPTION;" | mysql
+    echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'pass' WITH GRANT OPTION;" | mysql
+    service mysql stop
+
+
+
+    # echo -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost'; FLUSH PRIVILEGES;" | mysql
+    # echo -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE User='wordpress'; FLUSH PRIVILEGES;" | mysql
+    # echo -e "UPDATE mysql.user SET Password = PASSWORD('new_password') WHERE User = 'wordpress' AND Host = 'localhost'; FLUSH PRIVILEGES;" | mysql
+    # echo -e "UPDATE mysql.user SET Password = PASSWORD('azerty999') WHERE User = 'root'; FLUSH PRIVILEGES;" | mysql
+
+    # mariadb -e "UPDATE mysql.user SET Password = PASSWORD('azerty999') WHERE User = 'root'; FLUSH PRIVILEGES;"
+    # mariadb -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE User='wordpress'; FLUSH PRIVILEGES;"
+    # mariadb -e "UPDATE mysql.user SET Password = PASSWORD('new_password') WHERE User = 'wordpress' AND Host = 'localhost'; FLUSH PRIVILEGES;"
+    # mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'wordpress'@'srcs_adminer_1.srcs_wpsite' IDENTIFIED BY 'pass' WITH GRANT OPTION;"
+    # mariadb -e "GRANT ALL ON wordpress.* TO 'wordpress'@'localhost'; FLUSH PRIVILEGES;"
+
+
+    # mariadb -e "UPDATE mysql.user SET Password = PASSWORD('azerty999') WHERE User = 'root'"
+    # mariadb -e "CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'pass';"
+    # mariadb -e "GRANT ALL ON wordpress.* TO 'wordpress'@'localhost';"
+    # mariadb -e "FLUSH PRIVILEGES"
+fi
 
 exec mysqld_safe
---
+++ t
@@ -0,0 +1,12 @@
+try {
+    f1(x);
+    f2(x);
+    f3(x);
+    f4(x);
+    f5(x);
+    f6(x);
+    f7(x);
+    f8(x);
+} catch (std::execption& e) {
+    cout << e.what() << '\n';
+}
