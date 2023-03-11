66881e3683844a4659c47e1260e10c8f6b1056ef
sama <unlimit8.com@gmail.com>
2023-03-11 15:50:07 +0100

    static website

---
+++ srcs/requirements/bonus/static/conf/Documents/ventus/project1/java.js
@@ -0,0 +1,3 @@
+console.log(20);
+console.log(Math.random(15));
+console.log('how are you my friend'.length);
--- srcs/requirements/mariadb/tools/setup-mariadb.sh
+++ srcs/requirements/mariadb/tools/setup-mariadb.sh
@@ -10,10 +10,17 @@ sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 # else
     service mysql start
     mysql -e "CREATE DATABASE ${DB_NAME};"
+    mysql -e "CREATE DATABASE gitea;"
     mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}' IDENTIFIED BY '${DB_USER_PASS}' WITH GRANT OPTION;"
+    mysql -e "GRANT ALL PRIVILEGES ON gitea.* TO 'gitea' IDENTIFIED BY 'gitea' WITH GRANT OPTION;"
     mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}' WITH GRANT OPTION;"
     mysqladmin shutdown -p${DB_ROOT_PASS} 
 # fi
 
 
 mysqld_safe
+#    - GITEA__database__DB_TYPE=mysql
+#       - GITEA__database__HOST=mariadb:3306
+#       - GITEA__database__NAME=gitea
+#       - GITEA__database__USER=gitea
+#       - GITEA__database__PASSWD=gitea
