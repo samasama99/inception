866afc125e626aeca2f4c9b45faf23fca54e8f89
sama <unlimit8.com@gmail.com>
2023-03-11 21:53:08 +0100

    gitea

--- srcs/requirements/mariadb/tools/setup-mariadb.sh
+++ srcs/requirements/mariadb/tools/setup-mariadb.sh
@@ -8,13 +8,13 @@ sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 # then
 #    echo "Database '${DB_NAME}' already exists."
 # else
-    service mysql start
-    mysql -e "CREATE DATABASE ${DB_NAME};"
-    mysql -e "CREATE DATABASE gitea;"
-    mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}' IDENTIFIED BY '${DB_USER_PASS}' WITH GRANT OPTION;"
-    mysql -e "GRANT ALL PRIVILEGES ON gitea.* TO 'gitea' IDENTIFIED BY 'gitea' WITH GRANT OPTION;"
-    mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}' WITH GRANT OPTION;"
-    mysqladmin shutdown -p${DB_ROOT_PASS} 
+service mysql start
+mysql -e "CREATE DATABASE ${DB_NAME};"
+mysql -e "CREATE DATABASE gitea;"
+mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}' IDENTIFIED BY '${DB_USER_PASS}' WITH GRANT OPTION;"
+mysql -e "GRANT ALL PRIVILEGES ON gitea.* TO 'gitea' IDENTIFIED BY 'gitea' WITH GRANT OPTION;"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}' WITH GRANT OPTION;"
+mysqladmin shutdown -p${DB_ROOT_PASS} 
 # fi
 
 
